6e6d8a7be1fedda145dc56697e25b2b7754291f7
diegomirhan <mirhan.diego@gmail.com>
2021-10-18 20:11:09 -0300

    Update - 18/10

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ def main() -> None:
 
 
 def play(points: int) -> None:
-    df: int = int(input('Infme o nivel de dificuldade desejado [1, 2, 3]: '))
+    df: int = int(input('Informe o nivel de dificuldade desejado [1, 2, 3]: '))
     calc: Calculate = Calculate(df)
 
     print('Informe o resultado para a seguinte operação: ')
@@ -18,13 +18,12 @@ def play(points: int) -> None:
         points += 1
         print(f'Você tem {points} ponto(s).')
 
-    continue1: int = int(input('Deseja continuar o jogo? [1 - sim. 0 - não] '))
+    continue1: int = int(input('Deseja continuar o jogo? [1 - sim / 0 - não] '))
 
     if continue1:
         play(points)
     else:
-        print(f'Voce finalizou com {points} ponto(a).')
-        print('Até a próxima!')
+        print(f'Voce finalizou com {points} ponto(s). Até a próxima!')
 
 
 if __name__ == '__main__':
